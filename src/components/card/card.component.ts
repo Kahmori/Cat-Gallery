b40c38768ab98c69891086694420c92ed07b6e00
Karine Amorim <karineamor30@gmail.com>
2024-07-21 00:54:13 -0300

    api integration

--- src/components/card/card.component.ts
+++ src/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CatApiService } from '../../services/cat-api/cat-api.service';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  breeds: any[] = [];
 
-  constructor() { }
+  constructor(private catApiService: CatApiService) { }
 
   ngOnInit(): void {
-  }
+    this.catApiService.getList().subscribe((response: any) => {
+      this.breeds = response;
+      let filteredBreeds: any[] = [];
 
+      this.breeds.forEach(breed => {
+        this.catApiService.getCatDetails(breed.reference_image_id).subscribe((imageResponse: any) => {
+          breed.imageUrl = imageResponse.url;
+          if (!!breed.imageUrl) {
+            filteredBreeds.push(breed);
+          }
+        });
+      });
+      
+      setTimeout(() => {
+        this.breeds = filteredBreeds;
+      }, 0);
+    });
+  }
 }
--- src/services/cat-api/cat-api.service.ts
+++ src/services/cat-api/cat-api.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CatApiService {
+  private apiUrl = 'https://api.thecatapi.com/v1/breeds';
+  private detailedUrl = `https://api.thecatapi.com/v1/images/`;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getList() : Observable<any>{
+    return this.http.get(this.apiUrl);
+  }
+
+  getCatDetails(imageId: string) : Observable<any>{
+    const imageUrl = `${this.detailedUrl}${imageId}`;
+    return this.http.get(imageUrl);
+  }
 }
